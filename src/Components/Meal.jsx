6121f82afd294f614ffea0c4ed5e806438178659
abhilashadwivedi <abhilashadwivedi74@gmail.com>
2022-09-27 11:02:52 +0530

    created Meal.jsx

---
+++ src/Components/Meal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+const Meal= () =>{
+    return(
+        <>
+            <div className="main">
+                <div className="heading">
+                    <h1>Search Your Food Recipe</h1>
+                    <h4>Lorem blah blah blah</h4>
+                </div>
+                <div className="searchBox">
+                    <input type="search" className="search-box" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Meal;
