93586f8c1054e95e2824d0beb50fd13f8643b108
abhilashadwivedi <abhilashadwivedi74@gmail.com>
2022-09-28 09:31:32 +0530

    adding yt link

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Meal from "./Components/Meal";
 import './Components/style.css';
 import { Routes,Route} from "react-router-dom";
+import RecipeInfo from "./Components/RecipeInfo";
 function App() {
   return (
     <>
     <Routes>
     <Route path="/" element={<Meal/>} />
+    <Route path="/:MealId" element={<RecipeInfo />}/>
     </Routes>
     </>
   );
